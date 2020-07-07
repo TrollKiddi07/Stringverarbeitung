6fb61863b732f421c94a72d724d072dc3b20fd84
TrollKiddi07 <67582814+TrollKiddi07@users.noreply.github.com>
2020-07-07 12:37:43 +0200

    Create Stringverarbeitung.dart

---
+++ Stringverarbeitung.dart
@@ -0,0 +1,8 @@
+main() {
+String meinName = "Nils";
+  print("Mein Name ist $meinName");//Interpolation
+  
+  double komma = 6.0;
+  String kommazahl = komma.toString();//Casting
+  print("Ich habe einen Notendurchschnitt von $komma");//Interpolation
+}
